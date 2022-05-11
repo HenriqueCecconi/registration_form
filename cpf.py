6df8ef59772f796a7052ddc33713f3023da714ae
Henrique Cecconi <henrique.cecconi@gmail.com>
2022-05-11 17:38:31 -0300

    Since a person in Brasil can be a Fisical person or a Juridical Person, changed my class Cpf to Person and added the possibility of use a CPF or CNPJ.

--- cpf.py
+++ cpf.py
@@ -1,19 +1,29 @@
-# In Brasil, all valid CPF have the format ###.###.###-##
+# In Brasil, all valid CPF have the format ###.###.###-## and all valid CNPJ the format 
 # But it also has a matematic formula behind it to validate if a number is or is not valid
-from validate_docbr import CPF
-class Cpf:
-    def __init__(self, cpf):
-        self.cpf = cpf
-        self.validate_cpf()
+from validate_docbr import CPF,CNPJ
+class Person:
+    def __init__(self, document, doc_type):
+        self.document = document
+        self.doc_type = doc_type.lower()
+        self.validate_cpf() if self.doc_type == 'cpf' else self.validate_cnpj()
 
     def __str__(self):
-        return self.format_cpf()
+        return self.format_cpf() if self.doc_type == 'cpf' else self.format_cnpj()
 
     def format_cpf(self):
         mask = CPF()
-        return mask.mask(self.cpf)
+        return mask.mask(self.document)
+
+    def format_cnpj(self):
+        mask = CNPJ()
+        return mask.mask(self.document)
 
     def validate_cpf(self):
         validator = CPF()
-        if not validator.validate(self.cpf):
+        if not validator.validate(self.document):
             raise ValueError('Invalid CPF!') 
+
+    def validate_cnpj(self):
+        validator = CNPJ()
+        if not validator.validate(self.document):
+            raise ValueError('Invalid CNPJ!') 
