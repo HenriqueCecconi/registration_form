a7ba653ecbf09d62025866d5f790fcda16a8db17
Henrique Cecconi <henrique.cecconi@gmail.com>
2022-05-11 18:19:01 -0300

    Creating a factory to facilitate scalability, since I want to add even more documents to my system in the future, and ones not necessarily linked to a Person.

--- cpf.py
+++ cpf.py
@@ -1,27 +1,46 @@
 # In Brasil, all valid CPF have the format ###.###.###-## and all valid CNPJ the format 
 # But it also has a matematic formula behind it to validate if a number is or is not valid
 from validate_docbr import CPF,CNPJ
-class Person:
-    def __init__(self, document, doc_type):
+
+class Document:
+    @staticmethod
+    def create_document(document, doc_type):
+        doc_type = doc_type.lower()
+        if doc_type == 'cpf':
+            return Cpf(document)
+        elif doc_type == 'cnpj':
+            return Cnpj(document)
+        else:
+            raise ValueError('Not a valid document!')
+
+class Cpf:
+    def __init__(self, document):
         self.document = document
-        self.doc_type = doc_type.lower()
-        self.validate_cpf() if self.doc_type == 'cpf' else self.validate_cnpj()
+        self.validate_cpf()
 
     def __str__(self):
-        return self.format_cpf() if self.doc_type == 'cpf' else self.format_cnpj()
+        return self.format_cpf()
 
     def format_cpf(self):
         mask = CPF()
-        return mask.mask(self.document)
-
-    def format_cnpj(self):
-        mask = CNPJ()
         return mask.mask(self.document)
 
     def validate_cpf(self):
         validator = CPF()
         if not validator.validate(self.document):
             raise ValueError('Invalid CPF!') 
+
+class Cnpj:
+    def __init__(self, document):
+        self.document = document
+        self.validate_cnpj()
+
+    def __str__(self):
+        return self.format_cnpj()
+
+    def format_cnpj(self):
+        mask = CNPJ()
+        return mask.mask(self.document)
 
     def validate_cnpj(self):
         validator = CNPJ()
